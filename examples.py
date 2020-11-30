3f05e62b5b86826b21613f992591e818a119e759
Barak Korren <bkorren@redhat.com>
2020-11-30 10:51:02 +0200

    testing lecture: start of lecture code
    Code includes:
    - fibo() and power2() function stubs
    - simple test for power2()
    - stub failing test for fibo()
    
    Signed-off-by: Barak Korren <bkorren@redhat.com>

---
+++ examples.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""examples.py - Simple Python example functions for teaching PyTest
+"""
+
+def fibo(n):
+    """Calculate Fibonacchi numbers
+
+    :param int n: Which position in the series to return the number for
+    :returns: The Nth number in the Fibonecchi series
+    """
+
+
+def power2(n):
+    """Calculate powers of two
+
+    :param int n: Which number to calculate power of two for
+    :return: The Nth power of two
+    """
