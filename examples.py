c488e68252054394189d727478a6a5b38a207496
Barak Korren <bkorren@redhat.com>
2020-11-30 12:34:07 +0200

    testing lecture: Add function implementations
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- examples.py
+++ examples.py
@@ -2,12 +2,19 @@
 """examples.py - Simple Python example functions for teaching PyTest
 """
 
+
 def fibo(n):
     """Calculate Fibonacchi numbers
 
     :param int n: Which position in the series to return the number for
-    :returns: The Nth number in the Fibonecchi series
+    :returns: The Nth number in the Fibonacchi series
     """
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibo(n - 1) + fibo(n - 2)
 
 
 def power2(n):
@@ -16,3 +23,4 @@ def power2(n):
     :param int n: Which number to calculate power of two for
     :return: The Nth power of two
     """
+    return 2 ** n
